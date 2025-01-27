597a3809a6106389ea6b345d34cd0538eadebc68
MIRKOGUSMA <mirkogusma@gmail.com>
2025-01-27 08:27:24 +0100

    Update vite.config.js
    allower replit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Consente connessioni da altri dispositivi nella rete
+    host: true, // Permette connessioni da rete esterna
     port: 3000, // Porta del frontend
+    allowedHosts: [
+      'bba28ca-27ab-40da-9a1f-4d14a7bb953f-00-36j9y8iy69567.worf.repl.co' // Dominio Replit
+    ],
     proxy: {
-      "/api": {
-        target: "http://localhost:5001", // Indirizza le richieste al backend
+      '/api': {
+        target: 'http://localhost:5001', // Indirizza le richieste al backend
         changeOrigin: true,
       },
     },
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
 });
